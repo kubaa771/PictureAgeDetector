820a9dbc504f4c005ac7f2a5674dde504fa215d2
Jakub Iwaszek <kubaa771@gmail.com>
2021-06-04 15:34:21 +0200

    Initial Commit

---
+++ PictureAgeDetector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PictureAgeDetector
+//
+//  Created by Jakub Iwaszek on 04/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PictureAgeDetectorTests/PictureAgeDetectorTests.swift
@@ -0,0 +1,33 @@
+//
+//  PictureAgeDetectorTests.swift
+//  PictureAgeDetectorTests
+//
+//  Created by Jakub Iwaszek on 04/06/2021.
+//
+
+import XCTest
+@testable import PictureAgeDetector
+
+class PictureAgeDetectorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
